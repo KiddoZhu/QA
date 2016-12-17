bfd5a1de3dc3abae668de486c07a8871d65c3028
steve_gao <steve_gao@pku.edu.cn>
2016-12-17 23:16:33 +0800

    add templates

--- question.py
+++ question.py
@@ -4,7 +4,13 @@ import jieba
 import jieba.posseg as pseg
 import jieba.analyse 
 from nltk.parse.stanford import StanfordDependencyParser
-
+import codecs
+import re
+import sys
+from help_func import *
+# import debugger
+# reload(sys)
+# sys.setdefaultencoding('unicode')
 # JAR_PATH = "stanford-parser/stanford-parser.jar"
 # MODELS_PATH = "stanford-parser/stanford-parser-3.6.0-models.jar"
 
@@ -23,15 +29,42 @@ class Question(object) :
 		for k, v in kargs.items() :
 			setattr(self, k, v)
 		self.answerWordsList = []
+	
+	def setSentence(self, s):
+		assert(isinstance(s, unicode))
+		splitS = re.split(u'？|\t', s)
+		if len(splitS) == 1:
+			print 'no ? char'
+		s = splitS[0]
+		self.questionSentence = s
 
+	#Replace answer keyword
+	def addReKeyword(self, keyWord):
+		if self.questionSentence.find(keyWord) == -1:
+			return False
+		if keyWord not in list(self.wordsToken):
+			return False
+		pattern = [w.replace(keyWord, u'<any>') for w in self.wordsToken]
+		self.addTemp(pattern)
+		pattern = []
+		for w in self.wordsToken:
+			if w == keyWord:
+				break
+			pattern.append(w)
+		pattern.append(u'<any>')
+		self.addTemp(pattern)
+		return True
 
+	def addTemp(self, r):
+		if not hasattr(self, 'answerTemp'):
+			setattr(self, 'answerTemp', [])
+		self.answerTemp.append(r)
 
 def getQuestionType(wordsToken):
 	numberToken = [u'第几', u'多少', u'几大']
 	whatToken = [u'什么', u'哪个', u'哪一', u'哪所', u'哪种']
 	whoToken = [u'谁' ]
 	whereToken = [u'哪里']
-
 	for word in wordsToken:
 		# print word
 		if word in whatToken:
@@ -48,9 +81,60 @@ def getQuestionType(wordsToken):
 			return 'Where', ['ns']
 	return 'Unknown', []
 
-def getAnswerRe(pos, sentence):
-	print 'getAnswerRe.....'
 
+
+def getAnswerTemp(q):
+	sentence = q.questionSentence
+	# find keyword matching
+	keyWordMatching = [u'多少', u'几', u'谁', u'什么', 
+						u'哪个', u'哪一', u'哪所', u'哪种',
+						u'哪里', u'第几']
+	isWords = [u'是', u'成为', u'变成', u'由']
+	whereWords = [u'哪里', u'在哪', u'哪一']
+	for word in keyWordMatching:
+		q.addReKeyword(word)
+	
+	# some representative words we want to march
+	for w in isWords:
+		if w in q.wordsToken:
+			for w_is in isWords:
+				i = q.wordsToken.index(w)
+				before = q.wordsToken[:i]
+				pattern = [u'<any>', w_is]
+				pattern.extend(before)
+				q.addTemp(pattern)
+				pattern = [u'<any']
+				pattern.extend(before)
+				q.addTemp(pattern)
+				before.extend([w_is, u'<any>'])
+				q.addTemp(before)
+				# ignore sentence before ','
+				if sentence.find(u'，') != -1:
+					i_comma = sentence.rfind(u'，')
+					before_comma = sentence[:i_comma]
+					before_is = sentence[i_comma+1: sentence.rfind(w_is)]
+					before_comma = list(jieba.cut(before_comma))
+					before_is = list(jieba.cut(before_is))
+					before_comma.extend(before_is).extend([w_is, u'<any>'])
+					q.addTemp(before_comma)
+					
+					before_comma = sentence[:i_comma]
+					before_comma = list(jieba.cut(before_comma))
+					before_comma.extend([u'<any>', w_is]).extend(before_is)
+					q.addTemp(before_comma)
+	
+	if u'哪里' in q.wordsToken:
+		i = q.wordsToken.index(u'哪里')
+		before = q.wordsToken[:i]
+		after = q.wordsToken[i+1:]
+		if len(after) == 0:
+			pattern = [u'在', '<any>']
+			pattern.extend(before)
+			q.addTemp(pattern)
+			pattern = [u'位于', '<any>']
+			pattern.extend(before)
+			q.addTemp(pattern)
+			
 
 
 
@@ -63,34 +147,61 @@ class QuestionExtractor(object) :
 	def __call__(self, question) :
 		# print(question.questionSentence)
 		qSentence = question.questionSentence
-		assert(isinstance(qSentence, str))
+		# assert(isinstance(qSentence, str))
 
-		question.wordsToken = jieba.cut(qSentence)
-		question.posToken = pseg.cut(qSentence)
-		question.keyWordToken = jieba.analyse.extract_tags(qSentence, topK=5)
+		question.wordsToken = list(jieba.cut(qSentence))
+		question.posToken = list(pseg.cut(qSentence))
+		question.keyWordToken = list(jieba.analyse.extract_tags(qSentence, topK=5))
 		print ' '.join(question.keyWordToken)
 		# dependency = parser.parse(words).next()
 		print '/'.join(question.wordsToken)
-		for word, flag in question.posToken:
-			print('%s %s'%(word, flag))
+		# for word, flag in question.posToken:
+		# 	print('%s %s'%(word, flag))
 
-		question.questionTypem question.answerType = getQuestionType(question.wordsToken)
-		# question.answerType = getAnswerType(question.questionType)
-		question.answerRe = getAnswerRe(question.posToken, question.questionSentence)
+		question.questionType, question.answerType = getQuestionType(question.wordsToken)
+		getAnswerTemp(question)
+		my_print(question.answerTemp)
+		# print question.answerRe
 
 
 
 
 def matchAnswerWords(q, s):
-	print 'matchining'
+	# print 'matchining'
+	oriSent = q.questionSentence
+	tarSent = s
+	# print oriSent
+	# print tarSent
 
-def main():
-	s = '《华英字典》的作者是？'
-	question = Question(questionSentence=s)
+
+def doall(s):
+	# s = u'马尔代夫的第一大支柱产业是什么？	旅游业'
+	question = Question()
+	question.setSentence(s)
 	# setattr(question, 'questionSentence', '马尔代夫的第一大支柱产业是什么？')
 	extractor = QuestionExtractor()
 	extractor(question)
-	targetSentence = '《华英字典》的作者是马礼逊'
-	matchAnswerWords(question, targetSentence)
+	# print 'ALL the Templete'
+	#my_print(question.answerTemp)
+	#targetSentence = u'《华英字典》的作者是马礼逊'
+	#matchAnswerWords(question, targetSentence)
 
-main()
+def posText():
+	textName = 'test.txt'
+	f = codecs.open(textName, 'r', 'utf-8')
+	#while True:
+	#	line = f.readline()
+	#	if not line:
+	#		break
+		# print line
+	sampleQuestions = [u'甲午战争后，清政府签订了哪个不平等条约',
+		u'马尔代夫的第一大支柱产业是什么？',
+		u'《华英字典》的作者是？',
+		u'甲午战争爆发的标志是？',
+		]
+	for q in sampleQuestions:
+		doall(q)
+
+
+
+posText()
